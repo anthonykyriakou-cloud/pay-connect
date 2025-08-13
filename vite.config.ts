c1c3308664bd2f3614503201d1a36e92571dd2b2
anthonykyriakou-cloud <anthonykyriakou.github@gmail.com>
2025-08-13 22:45:28 +0300

    remove base change

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  
-  const base = mode === "development" ? "/pay-connect/" : "/";
-  
-  return {
-    server: {
-      host: "::",
-      port: 8080,
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react(), mode === "development" && componentTagger()].filter(
-      Boolean
-    ),
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    base,
-  };
-});
+  },
+}));
