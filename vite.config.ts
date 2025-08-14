7ab62525b182a21c2f2e0cfe9a38d2c3e6781c71
anthonykyriakou-cloud <anthonykyriakou.github@gmail.com>
2025-08-14 12:51:19 +0300

    re-add base pathname to ensure assets is added?

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/"
+  base: mode === "production" ? "/pay-connect/" : "/",  // Fix this!
 }));
